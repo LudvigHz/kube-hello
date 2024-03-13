f3b4a600b266b030cce78f8942e432c64bfb1988
Ludvig Hozman <ludvig123@gmail.com>
2024-03-13 18:16:22 +0100

    Init

---
+++ src/routes/KubeInfo.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { env } from '$env/dynamic/public';
+
+	let hostname = env.PUBLIC_HOSTNAME;
+</script>
+
+<div class="counter">
+	<h1>This page is served from <span><em>{hostname}</em></span></h1>
+	<div class="container">
+		<span class="help">Try reloading the page and see how the source host changes.</span>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.help {
+		font-size: 1em;
+	}
+</style>
---
+++ src/routes/config/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { env } from '$env/dynamic/public';
+</script>
+
+<svelte:head>
+	<title>Config</title>
+	<meta name="description" content="Kubernetes configuration" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>This applications config</h1>
+
+	<p>This apps environment is:</p>
+	<pre>{env.PUBLIC_ENVIRONMENT}</pre>
+
+	<br />
+
+	<p>My env is:</p>
+	<pre>{env.PUBLIC_MY_ENV}</pre>
+</div>
